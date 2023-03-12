d000cda0192f32a5231d72efc6b0ad019ad43dbe
Demon-stare <62430756+Demon-stare@users.noreply.github.com>
2023-03-12 11:29:57 +0000

    modified:   src/Feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -7,16 +7,12 @@ import { supabase } from './supabaseClient';
 const Feed = () => {
 
   const [session, setSession] = useState(null);
-
-  useEffect(() => {
+   
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
     });
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
+  
 
   return <div id="Home"> <h1>Hey {session.user.username} this is feed</h1> </div>;
 };
