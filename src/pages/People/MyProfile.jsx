7c5999165a38bb491282ea7f26ab731bb25750ce
Demon-stare <62430756+Demon-stare@users.noreply.github.com>
2023-04-08 17:41:33 +0000

    another fix ee

--- src/pages/People/MyProfile.jsx
+++ src/pages/People/MyProfile.jsx
@@ -8,8 +8,8 @@ export default function MyProfile() {
   const supabase = createClient('https://fjyhzorwyiggzjglhxbp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqeWh6b3J3eWlnZ3pqZ2xoeGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyMTc3MTIsImV4cCI6MTk5NDc5MzcxMn0.YpkAHXhwZrbDWROw4PiuFTe6ePKydhOTPaTJ2h9OIZk')
 
   let userdata;
-  loaddata();
   
+
   const loaddata = async (e) => {
 
     e.preventDefault();
@@ -24,6 +24,8 @@ export default function MyProfile() {
     catch (error) {
         alert(error.error_description || error.message);
     }
+
+    loaddata();
 
 
 }
